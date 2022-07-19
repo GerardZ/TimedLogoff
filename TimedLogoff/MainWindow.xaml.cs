519ad72b4f6c10f775559162b8eb884c3d7e0922
Gerard <gradtje@yahoo.com>
2022-07-19 11:00:58 +0200

    Command line added

--- TimedLogoff/MainWindow.xaml.cs
+++ TimedLogoff/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
+
+using System.Management;
 
 namespace TimedLogoff
 {
@@ -23,34 +12,62 @@ namespace TimedLogoff
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const int timerGrantStart = 300;
+        private int timerGrantStart = 150;
         
-        private int timerGrant = timerGrantStart;
+        private int timerGrant;
 
         private DispatcherTimer timer;
+
+        private EndMode endMode = EndMode.Sleep;
+
 
 
         public MainWindow()
         {
             InitializeComponent();
+            HandleCommandLineOptions();
+            timerGrant = timerGrantStart;
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timer_Tick;
             timer.Start();
         }
 
+        private void HandleCommandLineOptions()
+        {
+            string[] arguments = Environment.GetCommandLineArgs();
+            Console.WriteLine("GetCommandLineArgs: {0}", string.Join(", ", arguments));
+
+            foreach(string arg in arguments)
+            {
+                if (arg.ToLower() == "sleep") endMode = EndMode.Sleep;
+                if (arg.ToLower() == "shutdown") endMode = EndMode.Shutdown;
+                if (arg.ToLower() == "logoff") endMode = EndMode.Logoff;
+
+                int timerGrantStartValue;
+
+                if (Int32.TryParse(arg, out timerGrantStartValue)) timerGrantStart = timerGrantStartValue;
+
+                if (true) { }
+            }
+        }
+
         void timer_Tick(object sender, EventArgs e)
         {
-            mainLabel.Content = $"U wordt over {timerGrant--} seconden uitgelogd...";
-
-            progress.Value = timerGrant * 100/timerGrantStart;
-
-            if(timerGrant < 1)
+            if (timerGrant < 0)
             {
                 timer.Stop();
-                WindowsLogOff(true);
+                if (endMode == EndMode.Shutdown) WindowsShutdown(true);
+                if (endMode == EndMode.Sleep)    WindowsSleep();
+                if (endMode == EndMode.Logoff)   WindowsLogOff(true);
                 this.Close();
             }
+
+            if (endMode == EndMode.Shutdown) mainLabel.Content = $"Pc gaat over {timerGrant--} seconden shutdown...";
+            if (endMode == EndMode.Sleep) mainLabel.Content =    $"Pc gaat over {timerGrant--} seconden in sleep mode...";
+            if (endMode == EndMode.Logoff) mainLabel.Content =   $"U wordt over {timerGrant--} seconden uitgelogd...";
+
+            progress.Value = timerGrant * 100/timerGrantStart;
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
@@ -66,8 +83,62 @@ namespace TimedLogoff
 
         public static bool WindowsLogOff(bool force = false)
         {
-            if (force) ExitWindowsEx(0 | 0x00000004, 0);
+            if (force) return ExitWindowsEx(0 | 0x00000004, 0);
             return ExitWindowsEx(0, 0);
         }
+
+        // Sleep
+        // https://stackoverflow.com/questions/2079813/c-sharp-put-pc-to-sleep-or-hibernate
+        [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+
+        public static void WindowsSleep()
+        {
+            SetSuspendState(true, true, true);
+        }
+
+
+        // Shutdown
+        // https://stackoverflow.com/questions/102567/how-to-shut-down-the-computer-from-c-sharp
+        // force: https://social.msdn.microsoft.com/Forums/vstudio/en-US/93e33fe7-94aa-4161-aaf5-ddd0a7217b29/how-to-force-a-shutdown-of-window-even-if-connected-client?forum=wpf
+        public static void WindowsShutdown(bool force)
+        {
+            ManagementBaseObject mboShutdown = null;
+            ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+            mcWin32.Get();
+
+            // You can't shutdown without security privileges
+            mcWin32.Scope.Options.EnablePrivileges = true;
+            ManagementBaseObject mboShutdownParams = mcWin32.GetMethodParameters("Win32Shutdown");
+
+            // Flag 1 means we want to shut down the system
+            mboShutdownParams["Flags"] = "1";       // shutdown
+            mboShutdownParams["Flags"] = "5";       // shutdown forced
+            mboShutdownParams["Reserved"] = "0";
+
+            foreach (ManagementObject manObj in mcWin32.GetInstances())
+            {
+                mboShutdown = manObj.InvokeMethod("Win32Shutdown", mboShutdownParams, null);
+            }
+        }
+
+        // Settings
+        // https://www.c-sharpcorner.com/UploadFile/f9f215/windows-registry/
+
+        private void SaveSettings(Settings settings)
+        {
+
+        }
+    }
+    public class Settings
+    {
+        public string mode { get; set; }    
+    }
+
+    enum EndMode
+    {
+        Logoff,
+        Sleep,
+        Shutdown
     }
 }
